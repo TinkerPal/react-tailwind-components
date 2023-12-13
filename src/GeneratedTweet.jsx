f652c92885e316ab64b8769c0c19e62d78b6df85
TinkerPal <tinkerpi2023@gmail.com>
2023-12-12 20:45:42 -0500

    update

--- src/GeneratedTweet.jsx
+++ src/GeneratedTweet.jsx
@@ -23,6 +23,32 @@ export default function GeneratedTweet() {
 
       <div>
         <p className="font-bold text-lg my-4">Schedule post:</p>
+        <div className="flex space-x-4">
+          <div className="mb-8">
+            <p className="font-bold ">Date</p>
+            <input
+              type="date"
+              className="bg-white py-1 px-2 rounded-md"
+              id="scheduleDate"
+              name="scheduleDate"
+              min={new Date().toISOString().split("T")[0]} // Sets today as the minimum date
+              max={
+                new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
+                  .toISOString()
+                  .split("T")[0]
+              } // Sets 30 days from now as maximum date
+            />
+          </div>
+          <div>
+            <p className="font-bold ">Time</p>
+            <input
+              type="time"
+              className="bg-white py-1 px-2 rounded-md"
+              id="scheduleTime"
+              name="scheduleTime"
+            />
+          </div>
+        </div>
       </div>
 
       {/* copy up */}
