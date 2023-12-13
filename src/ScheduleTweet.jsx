c4aa744c22d3509593475b0b529304794f0e201a
TinkerPal <tinkerpi2023@gmail.com>
2023-12-12 20:13:09 -0500

    update

--- src/ScheduleTweet.jsx
+++ src/ScheduleTweet.jsx
@@ -20,11 +20,11 @@ export default function ScheduleTweet() {
           <div className="flex flex-row justify-between space-x-1 md:justify-normal">
             <button
               onClick={() => setIsManual(true)}
-              // className={`w-full md:w-auto flex justify-center items-center py-1 space-x-2 font-sans font-bold ${
-              //   isManual ? "text-white" : "text-black"
-              // } rounded-md md:px-2 ${isManual && "bg-cyan-700"} ${
-              //   !isManual && "border-2"
-              // } shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150`}
+              className={`w-full md:w-auto flex justify-center items-center py-1 space-x-2 font-sans font-bold ${
+                isManual ? "text-white" : "text-black"
+              } rounded-md md:px-2 ${isManual && "bg-cyan-700"} ${
+                !isManual && "border-2"
+              } shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150`}
             >
               <img
                 src={isManual ? manualWhite : manualBlack}
@@ -35,11 +35,11 @@ export default function ScheduleTweet() {
             </button>
             <button
               onClick={() => setIsManual(false)}
-              // className={`w-full md:w-auto flex justify-center items-center py-1 space-x-2 font-sans font-bold ${
-              //   !isManual ? "text-white" : "text-black"
-              // } rounded-md md:px-2 ${!isManual && "bg-cyan-700"} ${
-              //   isManual && "border-2"
-              // } shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150`}
+              className={`w-full md:w-auto flex justify-center items-center py-1 space-x-2 font-sans font-bold ${
+                !isManual ? "text-white" : "text-black"
+              } rounded-md md:px-2 ${!isManual && "bg-cyan-700"} ${
+                isManual && "border-2"
+              } shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150`}
             >
               <img src={isManual ? AIBlack : AIWhite} alt="" className="w-7 " />
               <span>AI Generated</span>
