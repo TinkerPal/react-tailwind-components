d7da66ec3b6d100300881c68bbe0195823aa288b
TinkerPal <tinkerpi2023@gmail.com>
2023-12-12 20:04:40 -0500

    update

--- src/ScheduleTweet.jsx
+++ src/ScheduleTweet.jsx
@@ -36,9 +36,9 @@ export default function ScheduleTweet() {
             <button
               onClick={() => setIsManual(false)}
               className={`w-full md:w-auto flex justify-center items-center py-1 space-x-2 font-sans font-bold ${
-                !isManual ? "text-white" : "text-black"
-              } rounded-md md:px-2 ${!isManual && "bg-cyan-700"} ${
-                isManual && "border-2"
+                isManual ? "text-white" : "text-black"
+              } rounded-md md:px-2 ${isManual && "bg-cyan-700"} ${
+                !isManual && "border-2"
               } shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150`}
             >
               <img src={isManual ? AIBlack : AIWhite} alt="" className="w-7 " />
