358ca57b70ad7d062883c0997a481f58cef51ce3
TinkerPal <tinkerpi2023@gmail.com>
2023-12-12 19:28:51 -0500

    update

--- src/GeneratedTweet.jsx
+++ src/GeneratedTweet.jsx
@@ -44,7 +44,7 @@ export default function GeneratedTweet() {
 
       <div className="font-bold text-lg">Select Sentiment</div>
       <select
-        name="How Many Iterations"
+        name="sentiment"
         id=""
         className="bg-white py-1 px-2 rounded-md mb-4"
       >
@@ -56,40 +56,15 @@ export default function GeneratedTweet() {
           </option>
         ))}
       </select>
-
-      <div className="pb-2 space-y-2 mb-10">
-        <div className="font-mono  text-3xl mt-10">Generated Contents</div>
-        <GeneratedItem tweets={tweets} />
+      <div className=" ">
+        <button className="w-full md:w-auto flex justify-center items-center p-2 space-x-2 font-sans font-bold text-white rounded-md  px-6 my-10 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150">
+          <span>Generate Contents</span>
+        </button>
       </div>
 
-      <div>
-        <p className="font-bold text-lg my-4">Schedule post:</p>
-        <div className="flex space-x-4">
-          <div className="mb-8">
-            <p className="font-bold ">Date</p>
-            <input
-              type="date"
-              className="bg-white py-1 px-2 rounded-md"
-              id="scheduleDate"
-              name="scheduleDate"
-              min={new Date().toISOString().split("T")[0]} // Sets today as the minimum date
-              max={
-                new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
-                  .toISOString()
-                  .split("T")[0]
-              } // Sets 30 days from now as maximum date
-            />
-          </div>
-          <div>
-            <p className="font-bold ">Time</p>
-            <input
-              type="time"
-              className="bg-white py-1 px-2 rounded-md"
-              id="scheduleTime"
-              name="scheduleTime"
-            />
-          </div>
-        </div>
+      <div className="pb-2 space-y-2 ">
+        <div className="font-mono  text-3xl mt-10">Generated Contents</div>
+        <GeneratedItem tweets={tweets} />
       </div>
 
       <div className="pb-6 ">
