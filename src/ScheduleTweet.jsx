9093325c4eacc93985161c8b3763a8e3b37415b4
TinkerPal <tinkerpi2023@gmail.com>
2023-12-13 04:58:38 -0500

    Update

--- src/ScheduleTweet.jsx
+++ src/ScheduleTweet.jsx
@@ -6,6 +6,7 @@ import manualBlack from "./assets/manualBlack.svg";
 import Banner from "./Banner";
 import GeneratedTweet from "./GeneratedTweet";
 import ManualTweet from "./ManualTweet";
+import TopNavbar from "./TopNavBar";
 
 export default function ScheduleTweet() {
   const [isManual, setIsManual] = useState(false);
@@ -13,39 +14,45 @@ export default function ScheduleTweet() {
   return (
     <div className="items-center  flex flex-col justify-start w-screen   bg-slate-100">
       {/* Inner container  */}
-      <div className="max-w-4xl mx-6 space-y-4 mt-12">
-        <Banner />
-
-        <div className="pt-20 space-y-2 ">
-          <div className="flex flex-row justify-between space-x-1 md:justify-normal">
-            <button
-              onClick={() => setIsManual(true)}
-              className={`w-full md:w-auto flex justify-center items-center py-1 space-x-2 font-sans font-bold ${
-                isManual ? "text-white" : "text-black"
-              } rounded-md md:px-2 ${isManual && "bg-cyan-700"} ${
-                !isManual && "border-2"
-              } shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150`}
-            >
-              <img
-                src={isManual ? manualWhite : manualBlack}
-                alt=""
-                className="w-7 "
-              />
-              <span>Manual Content</span>
-            </button>
-            <button
-              onClick={() => setIsManual(false)}
-              className={`w-full md:w-auto flex justify-center items-center py-1 space-x-2 font-sans font-bold ${
-                !isManual ? "text-white" : "text-black"
-              } rounded-md md:px-2 ${!isManual && "bg-cyan-700"} ${
-                isManual && "border-2"
-              } shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150`}
-            >
-              <img src={isManual ? AIBlack : AIWhite} alt="" className="w-7 " />
-              <span>AI Generated</span>
-            </button>
+      <div className="max-w-4xl mx-6 space-y-4 ">
+        <TopNavbar />
+        <div className="pt-8">
+          <Banner />
+          <div className="pt-20 space-y-2 ">
+            <div className="flex flex-row justify-between space-x-1 md:justify-normal">
+              <button
+                onClick={() => setIsManual(true)}
+                className={`w-full md:w-auto flex justify-center items-center py-1 space-x-2 font-sans font-bold ${
+                  isManual ? "text-white" : "text-black"
+                } rounded-md md:px-2 ${isManual && "bg-cyan-700"} ${
+                  !isManual && "border-2"
+                } shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150`}
+              >
+                <img
+                  src={isManual ? manualWhite : manualBlack}
+                  alt=""
+                  className="w-7 "
+                />
+                <span>Manual Content</span>
+              </button>
+              <button
+                onClick={() => setIsManual(false)}
+                className={`w-full md:w-auto flex justify-center items-center py-1 space-x-2 font-sans font-bold ${
+                  !isManual ? "text-white" : "text-black"
+                } rounded-md md:px-2 ${!isManual && "bg-cyan-700"} ${
+                  isManual && "border-2"
+                } shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150`}
+              >
+                <img
+                  src={isManual ? AIBlack : AIWhite}
+                  alt=""
+                  className="w-7 "
+                />
+                <span>AI Generated</span>
+              </button>
+            </div>
+            {isManual ? <ManualTweet /> : <GeneratedTweet />}
           </div>
-          {isManual ? <ManualTweet /> : <GeneratedTweet />}
         </div>
       </div>
     </div>
---
+++ src/TopNavBar.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+
+const TopNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className=" py-4">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <div className="flex-shrink-0">
+            <h1 className="text-black font-semibold text-lg">Logo</h1>
+          </div>
+          <div className="hidden md:flex space-x-8">
+            <a href="#" className="text-black hover:opacity-70">
+              Content Management
+            </a>
+            <a href="#" className="text-black hover:opacity-70">
+              Activities
+            </a>
+            <a href="#" className="text-black  hover:opacity-70">
+              Settings
+            </a>
+            <a href="#" className="text-black  hover:opacity-70">
+              Log Out
+            </a>
+          </div>
+          <div className="md:hidden flex">
+            <button
+              onClick={toggleNavbar}
+              className="text-black focus:outline-none"
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {isOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+        </div>
+        {isOpen && (
+          <div className="md:hidden mt-2">
+            <a href="#" className="block text-black py-2">
+              Content Management
+            </a>
+            <a href="#" className="block ttext-black py-2">
+              Activities
+            </a>
+            <a href="#" className="block text-black py-2">
+              Settings
+            </a>
+            <a href="#" className="block text-black py-2">
+              Log Out
+            </a>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default TopNavbar;
